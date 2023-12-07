43689b68cb1bf23281a78e1be76c16a6e2e53e99
Ehsun Amanolahi <ehsun.amanolahi@gmail.com>
2023-12-07 16:40:26 +0200

    GET /notes /notes/:id

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const { create } = require('../controllers/notes');
+const { create, byId, byOwner } = require('../controllers/notes');
 
 const router = express.Router();
 
@@ -8,7 +8,30 @@ router.post('/', async (req, res, next) => {
   try {
     const { body, user } = req;
     const note = await create(user, body);
+
     res.status(201).json({ message: 'CREATED', note });
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const { params, user } = req;
+    const note = await byId(user, { id: params.id });
+
+    res.status(200).json({ message: 'OK', note });
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.get('/', async (req, res, next) => {
+  try {
+    const { user } = req;
+    const notes = await byOwner(user);
+
+    res.status(200).json({ message: 'OK', notes });
   } catch (e) {
     next(e);
   }
--- src/routes/notes.test.js
+++ src/routes/notes.test.js
@@ -9,7 +9,9 @@ const {
 } = require('../db');
 const app = require('../app');
 const { table } = require('../models/notes');
+const { findOne: findOneUser } = require('../models/users');
 const { paramError } = require('../errors');
+const { decode } = require('../token');
 
 jest.mock('../config', () => {
   const actual = jest.requireActual('../config');
@@ -75,6 +77,59 @@ describe('/notes', () => {
 
       expect(response.status).toBe(paramError.status);
       expect(response.body.note).toBeFalsy();
+    });
+  });
+
+  describe('GET /notes/:id', () => {
+    it('should return a note by id', async () => {
+      const newNote = { text: 'text', public: false };
+
+      const { id } = (
+        await api
+          .post('/notes')
+          .set({ Authorization: `bearer ${token}` })
+          .send(newNote)
+      ).body.note;
+
+      const response = await api
+        .get(`/notes/${id}`)
+        .set({ Authorization: `bearer ${token}` });
+
+      expect(response.status).toBe(200);
+      expect(response.body.message).toBe('OK');
+      expect(response.body.note)
+        .toEqual(expect.objectContaining({ ...newNote, id }));
+    });
+  });
+
+  describe('GET /notes', () => {
+    it('should return all user notes', async () => {
+      const newNote = { text: 'text', public: false };
+
+      await api
+        .post('/notes')
+        .set({ Authorization: `bearer ${token}` })
+        .send(newNote);
+
+      await api
+        .post('/notes')
+        .set({ Authorization: `bearer ${token}` })
+        .send(newNote);
+
+      await api
+        .post('/notes')
+        .set({ Authorization: `bearer ${token}` })
+        .send(newNote);
+
+      const response = await api
+        .get('/notes')
+        .set({ Authorization: `bearer ${token}` });
+
+      expect(response.body.notes.length).toBe(3);
+
+      const user = await findOneUser(decode(token));
+
+      expect(response.body.notes.every(({ owner }) => owner === user.id)).toBe(true);
     });
   });
 });
