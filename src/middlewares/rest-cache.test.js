69a86d9c2419bf1bc09715408a33dc4178f78110
Ehsun Amanolahi <ehsun.amanolahi@gmail.com>
2023-12-29 12:16:14 +0200

    uncouple cache tests from notes

--- src/middlewares/rest-cache.test.js
+++ src/middlewares/rest-cache.test.js
@@ -1,260 +1,204 @@
 const supertest = require('supertest');
+const express = require('express');
 
 const app = require('../app');
-
-const {
-  validNewNote,
-  APIgetToken,
-  APIcreateNote,
-} = require('../jest/test-helpers');
-
-const { accessDenied } = require('../errors');
-
-const { userFromToken } = require('../controllers/login');
 const { getItem, removeItem, setItem } = require('../cache');
-const { cacheKey } = require('./rest-cache');
+const { cacheKey, cache } = require('./rest-cache');
+
+const success = { message: 'ok' };
+
+const router = express.Router();
+router.use(cache);
+router.use((req, res, next) => {
+  if (req.get('fail')) {
+    res.status(400).json({});
+  } else next();
+});
+router.get('/:id', (req, res) => { res.status(200).json(success); });
+router.get('/', (req, res) => { res.status(200).json(success); });
+router.post('/', (req, res) => { res.status(201).json(success); });
+router.put('/:id', (req, res) => { res.status(200).json(success); });
+router.delete('/:id', (req, res) => { res.status(200).json(success); });
+
+app.use('/test', router);
 
 const api = supertest(app);
 
-describe('/notes cache', () => {
+describe('cache middleware', () => {
+  beforeEach(async () => {
+    await removeItem('/test');
+    await removeItem('/test/id');
+  });
+
   describe('GET /:id', () => {
     it('should cache the results', async () => {
-      const token = await APIgetToken({ api });
-      const note = await APIcreateNote({ api, token });
-      const user = await userFromToken(token);
-      const key = cacheKey({ user, url: `/notes/${note.id}` });
+      const key = cacheKey({ url: '/test/id' });
 
-      await removeItem(key);
       expect(await getItem(key)).toBeFalsy();
 
-      await api
-        .get(`/notes/${note.id}`)
-        .set({ Authorization: `bearer ${token}` });
+      await api.get('/test/id');
 
-      expect((await getItem(key)).body.note).toEqual(note);
+      expect((await getItem(key)).body).toEqual(success);
     });
 
     it('should not cache, if fail', async () => {
-      const token = await APIgetToken({ api });
-      const note = await APIcreateNote({ api, token });
-      const user = await userFromToken(token);
-      const key = cacheKey({ user, url: `/notes/${note.id}` });
+      const key = cacheKey({ url: '/test/id' });
 
-      await removeItem(key);
       expect(await getItem(key)).toBeFalsy();
 
-      const response = await api
-        .get(`/notes/${note.id}`);
+      await api.get('/test/id')
+        .set({ fail: 1 });
 
-      expect(response.status).toBe(accessDenied.status);
       expect(await getItem(key)).toBeFalsy();
     });
 
     it('should use a cached value, if available', async () => {
-      const token = await APIgetToken({ api });
-      const note = await APIcreateNote({ api, token });
-      const user = await userFromToken(token);
-      const key = cacheKey({ user, url: `/notes/${note.id}` });
+      const key = cacheKey({ url: '/test/id' });
 
       const cached = {
         statusCode: 234,
-        body: { note: { ...note, text: 'foo', bar: 'baz' } },
+        body: 'foo',
       };
 
-      expect(cached).not.toEqual(note);
+      expect(cached).not.toEqual(success);
 
       await setItem(key, cached);
       expect(await getItem(key)).toEqual(cached);
 
-      const response = await api
-        .get(`/notes/${note.id}`)
-        .set({ Authorization: `bearer ${token}` });
+      const response = await api.get('/test/id');
 
       expect(response.statusCode).toEqual(cached.statusCode);
-      expect(response.body.note).toEqual(cached.body.note);
+      expect(response.body).toEqual(cached.body);
     });
   });
 
   describe('GET /', () => {
     it('should cache the results', async () => {
-      const token = await APIgetToken({ api });
-      const note = await APIcreateNote({ api, token });
-      const user = await userFromToken(token);
-      const key = cacheKey({ user, url: '/notes' });
+      const key = cacheKey({ url: '/test' });
 
-      await removeItem(key);
       expect(await getItem(key)).toBeFalsy();
 
-      await api
-        .get('/notes')
-        .set({ Authorization: `bearer ${token}` });
+      await api.get('/test');
 
-      expect((await getItem(key)).body.notes[0]).toEqual(note);
+      expect((await getItem(key)).body).toEqual(success);
     });
 
     it('should not cache, if fail', async () => {
-      const token = await APIgetToken({ api });
-      const user = await userFromToken(token);
-      const key = cacheKey({ user, url: '/notes' });
-
-      // TODO:
-      await removeItem(key);
+      const key = cacheKey({ url: '/test' });
 
       expect(await getItem(key)).toBeFalsy();
 
-      const response = await api
-        .get('/notes');
+      await api.get('/test')
+        .set({ fail: 1 });
 
-      expect(response.status).toBe(accessDenied.status);
       expect(await getItem(key)).toBeFalsy();
     });
 
     it('should use a cached value, if available', async () => {
-      const token = await APIgetToken({ api });
-      const note = await APIcreateNote({ api, token });
-      const user = await userFromToken(token);
-      const key = cacheKey({ user, url: '/notes' });
+      const key = cacheKey({ url: '/test' });
 
       const cached = {
         statusCode: 234,
-        body: { notes: [{ ...note, text: 'foo', bar: 'baz' }] },
+        body: 'foo',
       };
       await setItem(key, cached);
       expect(await getItem(key)).toEqual(cached);
 
-      const response = await api
-        .get('/notes')
-        .set({ Authorization: `bearer ${token}` });
+      const response = await api.get('/test');
 
       expect(response.statusCode).toEqual(cached.statusCode);
-      expect(response.body.notes[0]).toEqual(cached.body.notes[0]);
+      expect(response.body).toEqual(cached.body);
     });
   });
 
-  describe('POST /notes', () => {
-    it('should clear /notes cache', async () => {
-      const token = await APIgetToken({ api });
-      const user = await userFromToken(token);
-      const key = cacheKey({ user, url: '/notes' });
+  describe('POST /', () => {
+    it('should clear / cache', async () => {
+      const key = cacheKey({ url: '/test' });
 
       await setItem(key, 'foo');
       expect(await getItem(key)).toBeTruthy();
 
-      await api
-        .post('/notes')
-        .set({ Authorization: `bearer ${token}` })
-        .send(validNewNote());
+      await api.post('/test');
 
       expect(await getItem(key)).toBeFalsy();
     });
 
-    it('should not clear /notes cache, if fail', async () => {
-      const token = await APIgetToken({ api });
-      const user = await userFromToken(token);
-      const key = cacheKey({ user, url: '/notes' });
+    it('should not clear / cache, if fail', async () => {
+      const key = cacheKey({ url: '/test' });
 
       await setItem(key, 'foo');
       expect(await getItem(key)).toBeTruthy();
 
-      const response = await api
-        .post('/notes')
-        .send(validNewNote());
+      await api.post('/test')
+        .set({ fail: 1 });
 
-      expect(response.status).toBe(accessDenied.status);
       expect(await getItem(key)).toBe('foo');
     });
   });
 
-  describe('PUT /notes', () => {
-    it('should clear /notes cache & /notes/:id cache', async () => {
-      const token = await APIgetToken({ api });
-      const user = await userFromToken(token);
-      const note = await APIcreateNote({ api, token });
-      const key1 = cacheKey({ user, url: '/notes' });
-      const key2 = cacheKey({ user, url: `/notes/${note.id}` });
+  describe('PUT /', () => {
+    it('should clear / cache & /:id cache', async () => {
+      const key1 = cacheKey({ url: '/test' });
+      const key2 = cacheKey({ url: '/test/id' });
 
-      const modified = { ...note, text: 'foo' };
+      await setItem(key1, 'foo1');
+      await setItem(key2, 'foo2');
+      expect(await getItem(key1)).toBe('foo1');
+      expect(await getItem(key2)).toBe('foo2');
 
-      await setItem(key1, 'foo');
-      await setItem(key2, 'foo');
-      expect(await getItem(key1)).toBe('foo');
-      expect(await getItem(key2)).toBe('foo');
-
-      await api
-        .put(`/notes/${note.id}`)
-        .set({ Authorization: `bearer ${token}` })
-        .send(modified);
+      await api.put('/test/id');
 
       expect(await getItem(key1)).toBeFalsy();
       expect(await getItem(key2)).toBeFalsy();
     });
 
-    it('should not clear /notes cache & /notes/:id cache, if fail', async () => {
-      const token = await APIgetToken({ api });
-      const user = await userFromToken(token);
-      const note = await APIcreateNote({ api, token });
-      const key1 = cacheKey({ user, url: '/notes' });
-      const key2 = cacheKey({ user, url: `/notes/${note.id}` });
+    it('should not clear / cache & /:id cache, if fail', async () => {
+      const key1 = cacheKey({ url: '/test' });
+      const key2 = cacheKey({ url: '/test/id' });
 
-      const modified = { ...note, text: 'foo' };
+      await setItem(key1, 'foo1');
+      await setItem(key2, 'foo2');
+      expect(await getItem(key1)).toBe('foo1');
+      expect(await getItem(key2)).toBe('foo2');
 
-      await setItem(key1, 'foo');
-      await setItem(key2, 'foo');
-      expect(await getItem(key1)).toBe('foo');
-      expect(await getItem(key2)).toBe('foo');
+      await api.put('/test/id')
+        .set({ fail: 1 });
 
-      const response = await api
-        .put(`/notes/${note.id}`)
-        .send(modified);
-
-      expect(response.status).toBe(accessDenied.status);
-      expect(await getItem(key1)).toBe('foo');
-      expect(await getItem(key2)).toBe('foo');
+      expect(await getItem(key1)).toBe('foo1');
+      expect(await getItem(key2)).toBe('foo2');
     });
   });
 
-  describe('DELETE /notes', () => {
-    it('should clear /notes cache & /notes/:id cache', async () => {
-      const token = await APIgetToken({ api });
-      const user = await userFromToken(token);
-      const note = await APIcreateNote({ api, token });
-      const key1 = cacheKey({ user, url: '/notes' });
-      const key2 = cacheKey({ user, url: `/notes/${note.id}` });
+  describe('DELETE /', () => {
+    it('should clear / cache & /:id cache', async () => {
+      const key1 = cacheKey({ url: '/test' });
+      const key2 = cacheKey({ url: '/test/id' });
 
-      await setItem(key1, 'foo');
-      await setItem(key2, 'foo');
-      expect(await getItem(key1)).toBe('foo');
-      expect(await getItem(key2)).toBe('foo');
+      await setItem(key1, 'foo1');
+      await setItem(key2, 'foo2');
+      expect(await getItem(key1)).toBe('foo1');
+      expect(await getItem(key2)).toBe('foo2');
 
-      await api
-        .delete(`/notes/${note.id}`)
-        .set({ Authorization: `bearer ${token}` });
+      await api.delete('/test/id');
 
       expect(await getItem(key1)).toBeFalsy();
       expect(await getItem(key2)).toBeFalsy();
     });
 
-    it('should not clear /notes cache & /notes/:id cache, if fail', async () => {
-      const token = await APIgetToken({ api });
-      const user = await userFromToken(token);
-      const note = await APIcreateNote({ api, token });
-      const key1 = cacheKey({ user, url: '/notes' });
-      const key2 = cacheKey({ user, url: `/notes/${note.id}` });
+    it('should not clear / cache & /:id cache, if fail', async () => {
+      const key1 = cacheKey({ url: '/test' });
+      const key2 = cacheKey({ url: '/test/id' });
 
-      const modified = { ...note, text: 'foo' };
+      await setItem(key1, 'foo1');
+      await setItem(key2, 'foo2');
+      expect(await getItem(key1)).toBe('foo1');
+      expect(await getItem(key2)).toBe('foo2');
 
-      await setItem(key1, 'foo');
-      await setItem(key2, 'foo');
-      expect(await getItem(key1)).toBe('foo');
-      expect(await getItem(key2)).toBe('foo');
+      await api.delete('/test/id')
+        .set({ fail: 1 });
 
-      const response = await api
-        .delete(`/notes/${note.id}`)
-        .send(modified);
-
-      expect(response.status).toBe(accessDenied.status);
-      expect(await getItem(key1)).toBe('foo');
-      expect(await getItem(key2)).toBe('foo');
+      expect(await getItem(key1)).toBe('foo1');
+      expect(await getItem(key2)).toBe('foo2');
     });
   });
 });
