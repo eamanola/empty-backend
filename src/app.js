6d6172ef0ab7c71ed5df07035fd3fa1c2234e5c8
Ehsun Amanolahi <ehsun.amanolahi@gmail.com>
2024-06-24 18:30:22 +0300

    logging, /health

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
+
 const { usersRouter, errorHandler, errors } = require('./lib');
+const { NODE_ENV } = require('./config');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+if (NODE_ENV !== 'test') {
+  app.use(morgan('tiny'));
+}
+
+app.get('/health', (req, res) => { res.status(200).send('OK'); });
 
 app.use(usersRouter);
 
